e55338328cbc648276f4001783f23fa07c47e160
escritos <escritoyane@gmail.com>
2024-07-16 14:41:24 +0900

    board

--- resources/views/boards/index.blade.php
+++ resources/views/boards/index.blade.php
@@ -1,4 +1,31 @@
 @extends('boards.layout')
 @section('content')
-게시판 메인 화면입니다.
+
+    <h2 class="mt-4 mb-3">게시판 목록</h2>
+    <div style="text-align:right;">
+        <button class="text-xl">등록</button></a>
+    </div>
+    
+    <table class="table table-striped table-hover">
+        <colgroup>
+            <col width="10%"/>
+            <col width="15%"/>
+            <col width="45%"/>
+            <col width="15%"/>
+            <col width="15%"/>
+        </colgroup>
+        <thead>
+        <tr>
+            <th scope="col">번호</th>
+            <th scope="col">이름</th>
+            <th scope="col">제목</th>
+            <th scope="col">조회수</th>
+            <th scope="col">등록일</th>
+        </tr>
+        </thead>
+        <tbody>
+        
+        </tbody>
+    </table>
+
 @endsection
