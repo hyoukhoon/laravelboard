49b6523fffbc93d5feb600797879d7d67d4df275
escritos <escritoyane@gmail.com>
2024-07-16 10:12:27 +0900

    board

---
+++ app/Http/Controllers/BoardController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Board;
+use Illuminate\Http\Request;
+
+class BoardController extends Controller
+{
+    public function index(){
+        return view('boards.index');
+    }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//게시판
+Route::get('/boards', [BoardController::class, 'index'])->name('boards.index');
