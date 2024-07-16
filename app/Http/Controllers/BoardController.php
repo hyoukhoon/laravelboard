62e256f01913b8e9e13a2b12ab55cd945f1bf48e
escritos <escritoyane@gmail.com>
2024-07-16 15:05:48 +0900

    board

--- app/Http/Controllers/BoardController.php
+++ app/Http/Controllers/BoardController.php
@@ -8,6 +8,7 @@ class BoardController extends Controller
 {
     public function index(){
         $boards = Board::orderBy('bid','desc');
+        print_r($boards);
         return view('boards.index', compact('boards'));
     }
 }
