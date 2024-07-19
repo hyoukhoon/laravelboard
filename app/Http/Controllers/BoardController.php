7d620f9c7312aed242693edd429c27689f97e50d
escritos <escritoyane@gmail.com>
2024-07-19 14:32:49 +0900

    board2

--- app/Http/Controllers/BoardController.php
+++ app/Http/Controllers/BoardController.php
@@ -9,7 +9,7 @@ class BoardController extends Controller
 {
     public function index(){
         $boards = Board::orderBy('bid','desc')->paginate(20);
-        return view('boards.index', ['boards' => $boards, 'boardTitle' => '게시판 목록']);
+        return view('boards.index', compact('boards'));
     }
 
     public function show($bid,$page)
@@ -19,13 +19,13 @@ class BoardController extends Controller
         $boards->content = htmlspecialchars_decode($boards->content);
         $boards->pagenumber = $page??1;
 
-        return view('boards.view', ['boards' => $boards, 'boardTitle' => '게시판 보기']);
+        return view('boards.view', ['boards' => $boards]);
     }
 
     public function write()
     {
         if(auth()->check()){
-            return view('boards.write', ['boardTitle' => '게시판 등록']);
+            return view('boards.write');
         }else{
             return redirect()->back()->withErrors('로그인 하십시오.');
         }
--- resources/views/boards/write.blade.php
+++ resources/views/boards/write.blade.php
@@ -1,6 +1,23 @@
 @extends('boards.layout')
 
-
+@section('header')
+    <div class="d-flex flex-column flex-md-row align-items-center pb-3 mb-4 border-bottom">
+        <span class="fs-4">게시판 글쓰기</span>
+        <nav class="d-inline-flex mt-2 mt-md-0 ms-md-auto">
+        @guest()
+            <a href="{{route('auth.login')}}" class="text-xl">로그인</a> / 
+            <a href="{{route('auth.signup')}}" class="text-xl">회원가입</a>
+        @endguest
+        @auth()
+            <form action="/logout" method="post" class="inline-block">
+                @csrf
+                <span class="text-xl text-blue-500">{{auth()->user()->userid}}</span> / 
+                <a href="/logout"><button class="text-xl">로그아웃</button></a>
+            </form>
+        @endauth
+        </nav>
+    </div>
+@endsection
 
 @section('content')
 <br />
