04094e1042351a1c2fba3239819bd2b841f8a465
escritos <escritoyane@gmail.com>
2024-07-19 13:55:56 +0900

    board2

--- resources/views/boards/layout.blade.php
+++ resources/views/boards/layout.blade.php
@@ -14,7 +14,24 @@
 <body>
 <div class="container">
     <header>
-        @yield('header')
+        @section('header')
+            <div class="d-flex flex-column flex-md-row align-items-center pb-3 mb-4 border-bottom">
+                <span class="fs-4">게시판 목록</span>
+                <nav class="d-inline-flex mt-2 mt-md-0 ms-md-auto">
+                @guest()
+                    <a href="{{route('auth.login')}}" class="text-xl">로그인</a> / 
+                    <a href="{{route('auth.signup')}}" class="text-xl">회원가입</a>
+                @endguest
+                @auth()
+                    <form action="/logout" method="post" class="inline-block">
+                        @csrf
+                        <span class="text-xl text-blue-500">{{auth()->user()->userid}}</span> / 
+                        <a href="/logout"><button class="text-xl">로그아웃</button></a>
+                    </form>
+                @endauth
+                </nav>
+            </div>
+        @endsection
     </header>
     @yield('content')
 </div>
