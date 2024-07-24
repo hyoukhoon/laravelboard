b3c1bd855aacacc0cc5c563866d3947d7bf0b5c7
hyoukhoon <hyoukhoon@gmail.com>
2024-07-24 15:39:01 +0900

    files

---
+++ database/migrations/2024_07_24_063152_create_file_tables_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('file_tables', function (Blueprint $table) {
+		$table->id();
+		$table->integer('pid');
+		$table->string('multi', 100);
+		$table->string('email', 100);
+		$table->string('filename', 100);
+		$table->tinyInteger('status')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('file_tables');
+    }
+};
