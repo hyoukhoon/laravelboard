a05fd4064a2dfd3b752c258b43a4e565b05c0808
escritos <escritoyane@gmail.com>
2024-07-25 12:58:35 +0900

    board5

--- resources/views/boards/view.blade.php
+++ resources/views/boards/view.blade.php
@@ -10,19 +10,19 @@
     <table class="table table-striped table-hover">
         <tbody>
             <tr>
-                <th width="100">제목</th>
+                <th width="200">제목</th>
                 <td>{{ $boards->subject }}</td>
             </tr>
             <tr>
                 <td colspan="2">글쓴이 : {{ $boards->userid }} / 조회수 : {{ number_format($boards->cnt) }} / 등록일 : {{ $boards->regdate }}</td>
             </tr>
             <tr>
-                <th width="100">내용</th>
+                <th width="200">내용</th>
                 <td>{!! nl2br($boards->content) !!}</td>
             </tr>
             @if($attaches)
             <tr>
-                <th width="100">첨부 이미지</th>
+                <th width="200">첨부 이미지</th>
                 <td>
                     @foreach ($attaches as $att)
                     <div id='af_{{ $att->id }}' class='card h-100' style='width:120px;margin-right: 10px;margin-bottom: 10px;'><img src='/images/{{ $att->filename }}' width='100' /></div>
