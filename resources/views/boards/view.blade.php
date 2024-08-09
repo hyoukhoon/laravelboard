4b8592747429054ad586e33de15aa7052404048e
escritos <escritoyane@gmail.com>
2024-08-09 14:59:16 +0900

    memo5

--- resources/views/boards/view.blade.php
+++ resources/views/boards/view.blade.php
@@ -167,14 +167,11 @@
             type: 'POST',
             success: function (return_data) {
                 var html = "<img src='/images/"+return_data.fn+"' style='max-width:100%;height:88px;'>";
-                console.log("170:"+modimemoid);
                 if(modimemoid>0){
-                    console.log("172:"+html);
                     $("#modi_memo_image_view").html(html);
                     $("#modi_memo_image_view").show();
                     $("#modimemoimg").hide();
                 }else{
-                    console.log("176:"+html);
                     $("#memo_image_view").html(html);
                     $("#memo_image_view").show();
                     $("#attmemoimg").hide();
