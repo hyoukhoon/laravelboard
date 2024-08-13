cff03f8e04202c89a2415577df7f1c99b25eee77
escritos <escritoyane@gmail.com>
2024-08-13 10:47:53 +0900

    memo7

--- resources/views/boards/view.blade.php
+++ resources/views/boards/view.blade.php
@@ -49,7 +49,7 @@
     <div id="reply">
         @foreach ($memos as $m)
         @if ($m->pid)
-            <div class="d-flex" id="{{ 'memolist_'.$m->id }}">
+            <div class="d-flex">
                 <div class="p-2">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-return-right" viewBox="0 0 16 16">
                     <path fill-rule="evenodd" d="M1.5 1.5A.5.5 0 0 0 1 2v4.8a2.5 2.5 0 0 0 2.5 2.5h9.793l-3.347 3.346a.5.5 0 0 0 .708.708l4.2-4.2a.5.5 0 0 0 0-.708l-4-4a.5.5 0 0 0-.708.708L13.293 8.3H3.5A1.5 1.5 0 0 1 2 6.8V2a.5.5 0 0 0-.5-.5z"></path>
@@ -76,7 +76,7 @@
                                 </tr>
                             </tbody></table>
                         </div>
-                        <div class="card-body">
+                        <div class="card-body" id="{{ 'memolist_'.$m->id }}">
                             <p class="card-text">{!! nl2br($m->memo) !!}</p>
                             @auth()
                             <span style="float:right;">
