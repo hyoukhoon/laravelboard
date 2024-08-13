2e54786862f0fe7f6a719346f52d1fdb487808d5
escritos <escritoyane@gmail.com>
2024-08-13 12:43:32 +0900

    memo7

--- app/Helpers.php
+++ app/Helpers.php
@@ -21,9 +21,9 @@ function disptime($regdate){
 
 function dispmemo($memo_cnt, $memo_date){
     if((time()-strtotime($memo_date))<86400){
-        return "<span style='color:red;'>".$memo_cnt."</span>";
+        return "<span style='color:red;'>[".$memo_cnt."]</span>";
     }else{
-        return "<span>".$memo_cnt."</span>";
+        return "<span>[".$memo_cnt."]</span>";
     }
 }
 
