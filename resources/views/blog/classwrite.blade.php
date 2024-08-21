e50f35e2a0b11e45a178bcd87a4d53e488e297fe
escritos <escritoyane@gmail.com>
2024-08-21 13:56:57 +0900

    blog2

--- resources/views/blog/classwrite.blade.php
+++ resources/views/blog/classwrite.blade.php
@@ -35,10 +35,10 @@
                 <form action="">
   
                   <h4>강의실에 강좌 올리기</h4>
-                  <p>좋은 글을 남깁니다.</p>
+                  <p style="float:right;">좋은 글을 남깁니다.</p>
                   <div class="row">
                     <div class="col form-group">
-                      <input name="website" type="text" class="form-control" placeholder="제목을 등록하세요.">
+                      <input name="website" type="text" class="form-control" placeholder="제목을 입력하세요.">
                     </div>
                   </div>
                   <div class="row">
