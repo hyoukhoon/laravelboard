a9f70682151975ca480f5965672d02b79dda089c
escritos <escritoyane@gmail.com>
2024-08-22 13:25:18 +0900

    blog3

--- app/Http/Controllers/ClassController.php
+++ app/Http/Controllers/ClassController.php
@@ -4,13 +4,18 @@ namespace App\Http\Controllers;
 use App\Models\Classrooms;
 use App\Models\FileTables;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 
 class ClassController extends Controller
 {
     public function classroom(){
-        return view('blog.classroom');
+        $contents = DB::table('classrooms')
+                    ->join('file_tables', 'classrooms.id', '=', 'file_tables.pid')
+                    ->select('classrooms.*', 'file_tables.filename')
+                    ->get();
+        return view('blog.classroom', ['contents' => $contents]);
     }
 
     public function classview(){
@@ -32,6 +37,10 @@ class ClassController extends Controller
 
     public function classcreate(Request $request)
     {
+        if(Auth::user()->memberlevels<10){
+            return redirect()->back()->withErrors('권한이 없습니다.');
+            exit;
+        }
         $form_data = array(
             'subject' => $request->subject,
             'contents' => $request->content,
@@ -42,7 +51,7 @@ class ClassController extends Controller
         if(auth()->check()){
             $rs=Classrooms::create($form_data);
             FileTables::where('pid', $request->pid)->where('userid', Auth::user()->userid)->wherein('code',['classroom'])->update(array('pid' => $rs->id));
-            return response()->json(array('msg'=> "succ", 'bid'=>$rs->bid), 200);
+            return response()->json(array('msg'=> "succ", 'bid'=>$rs->id), 200);
         }
     }
 
--- resources/views/blog/classroom.blade.php
+++ resources/views/blog/classroom.blade.php
@@ -34,21 +34,23 @@
             <div class="container">
               <div class="row gy-4">
 
+
+                @foreach ($contents as $cs)
                 <div class="col-lg-6">
                   <article class="position-relative h-100">
 
                     <div class="post-img position-relative overflow-hidden">
-                      <img src="/assets/img/blog/blog-1.jpg" class="img-fluid" alt="">
-                      <span class="post-date">December 12</span>
+                      <img src="/images/{{ $cs->filename }}" class="img-fluid" alt="">
+                      <span class="post-date">{{ $cs->created_at }}</span>
                     </div>
 
                     <div class="post-content d-flex flex-column">
 
-                      <h3 class="post-title">Dolorum optio tempore voluptas dignissimos cumque fuga qui quibusdam quia</h3>
+                      <h3 class="post-title">{{ $cs->subject }}</h3>
 
                       <div class="meta d-flex align-items-center">
                         <div class="d-flex align-items-center">
-                          <i class="bi bi-person"></i> <span class="ps-2">John Doe</span>
+                          <i class="bi bi-person"></i> <span class="ps-2">{{ $cs->userid }}</span>
                         </div>
                         <span class="px-3 text-black-50">/</span>
                         <div class="d-flex align-items-center">
@@ -68,6 +70,7 @@
 
                   </article>
                 </div><!-- End post list item -->
+                @endforeach
 
                 <div class="col-lg-6">
                   <article class="position-relative h-100">
