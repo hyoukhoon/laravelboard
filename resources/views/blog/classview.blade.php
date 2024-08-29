dc44526274946453f42341c46bf5391e5262cea9
escritos <escritoyane@gmail.com>
2024-08-29 13:56:42 +0900

    blog6

--- resources/views/blog/classview.blade.php
+++ resources/views/blog/classview.blade.php
@@ -96,20 +96,18 @@
                         <div class="flex-fill" style="width:100%">
                             <div class="card mt-2">
                                 <div class="card-header">
-                                    <table>
-                                        <tbody><tr class="align-middle">
-                                            <td rowspan="2" class="pr-2">
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-card-text" viewBox="0 0 16 16">
+                                    <table width="100%">
+                                        <tbody>
+                                        <tr>
+                                            <td width="40">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-card-text" viewBox="0 0 16 16">
                                                     <path d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2z"/>
                                                     <path d="M3 5.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5M3 8a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9A.5.5 0 0 1 3 8m0 2.5a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5"/>
                                                 </svg>
                                             </td>
-                                            <td class="ml">{{ $m->userid }}</td>
-                                        </tr>
-                                        <tr>
-                                            <td>
+                                            <td>{{ $m->userid }}</td>
+                                            <td align="right">
                                                 <font size="2">{{ disptime($m->created_at) }}</font> 
-                                                <span style="cursor:pointer" onclick="#"></span>
                                             </td>
                                         </tr>
                                     </tbody></table>
