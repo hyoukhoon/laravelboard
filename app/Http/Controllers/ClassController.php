827e4910b575a35f0bbad4872f0100f1032fb95b
escritos <escritoyane@gmail.com>
2024-08-30 17:10:24 +0900

    blog6

--- app/Http/Controllers/ClassController.php
+++ app/Http/Controllers/ClassController.php
@@ -236,7 +236,7 @@ class ClassController extends Controller
         ]);
 
         if(auth()->check()){
-            // $image = $request->file('file');
+            $image = $request->file('file');
             $new_name = rand().'_'.time().'.'.$image->getClientOriginalExtension();
             // $image->move(public_path('images'), $new_name);
             $new_name=Storage::putFileAs('images', $request->file('file'), $new_name);
