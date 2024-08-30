b993260a31e0b1b2c12cb7c3a7a65cce40ae17a9
escritos <escritoyane@gmail.com>
2024-08-30 16:57:30 +0900

    blog6

--- app/Http/Controllers/ClassController.php
+++ app/Http/Controllers/ClassController.php
@@ -239,7 +239,7 @@ class ClassController extends Controller
             // $image = $request->file('file');
             // $new_name = rand().'_'.time().'.'.$image->getClientOriginalExtension();
             // $image->move(public_path('images'), $new_name);
-            $new_name=Storage::putFileAs('images', $request->file('file'), date("YmdHis"));
+            $new_name=Storage::putFileAs('public', $request->file('file'), date("YmdHis"));
             $pid = $request->modimemoid?$request->modimemoid:$request->pid;
             $fid = rand();
             $form_data = array(
