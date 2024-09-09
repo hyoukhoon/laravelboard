38e57a4b08f7ef38b4bfcebd395a66bf7bddc081
escritos <escritoyane@gmail.com>
2024-09-09 14:47:32 +0900

    blog10

--- app/Http/Controllers/MemberController.php
+++ app/Http/Controllers/MemberController.php
@@ -23,10 +23,7 @@ class MemberController extends Controller
         $validator = Validator::make($request->all(), [
             'username' => 'required',
             'email' => 'required',
-            'password' => ['required', 'confirmed', Password::min(8)
-                                                            ->letters()
-                                                            ->numbers()
-                                                            ->symbols()]
+            'password' => ['required', 'confirmed', Password::min(8)->letters()->numbers()->symbols()]
         ]);
 
         if ($validator->fails()) {
