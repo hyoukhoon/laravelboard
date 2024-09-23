c7a4735495a42c54a1463638d68f10674093129d
escritos <escritoyane@gmail.com>
2024-09-23 16:35:13 +0900

    blog13

--- resources/views/adminarea/classroom.blade.php
+++ resources/views/adminarea/classroom.blade.php
@@ -30,10 +30,31 @@
                     </select>
                   </td>
                   <td>
-                    <button type="button" class="btn btn-primary">분류 관리</button>
+                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                      분류등록
+                    </button>
                   </td>
                 </tr>
               </table>
+
+              <!-- Modal -->
+              <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                      ...
+                    </div>
+                    <div class="modal-footer">
+                      <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                      <button type="button" class="btn btn-primary">Save changes</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
               
               <!-- Table with stripped rows -->
               <table class="table datatable">
