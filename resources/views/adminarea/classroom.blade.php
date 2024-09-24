b4961329f9a62f608d7a43c44fee4a0608cf2bea
escritos <escritoyane@gmail.com>
2024-09-24 16:30:49 +0900

    blog14

--- resources/views/adminarea/classroom.blade.php
+++ resources/views/adminarea/classroom.blade.php
@@ -12,60 +12,57 @@
     </div><!-- End Page Title -->
 
     <section class="section">
-      
-      <!-- News & Updates Traffic -->
-      <div class="card">
-        <div class="filter">
-          <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
-          <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
-            <li class="dropdown-header text-start">
-              <h6>Filter</h6>
-            </li>
 
-            <li><a class="dropdown-item" href="#">Today</a></li>
-            <li><a class="dropdown-item" href="#">This Month</a></li>
-            <li><a class="dropdown-item" href="#">This Year</a></li>
-          </ul>
-        </div>
-
-        <div class="card-body pb-0">
-          <h5 class="card-title">News &amp; Updates <span>| Today</span></h5>
-
-          <div class="news">
-            <div class="post-item clearfix">
-              <img src="/admin/assets/img/news-1.jpg" alt="">
-              <h4><a href="#">Nihil blanditiis at in nihil autem</a></h4>
-              <p>Sit recusandae non aspernatur laboriosam. Quia enim eligendi sed ut harum...</p>
-            </div>
-
-            <div class="post-item clearfix">
-              <img src="/admin/assets/img/news-2.jpg" alt="">
-              <h4><a href="#">Quidem autem et impedit</a></h4>
-              <p>Illo nemo neque maiores vitae officiis cum eum turos elan dries werona nande...</p>
-            </div>
-
-            <div class="post-item clearfix">
-              <img src="/admin/assets/img/news-3.jpg" alt="">
-              <h4><a href="#">Id quia et et ut maxime similique occaecati ut</a></h4>
-              <p>Fugiat voluptas vero eaque accusantium eos. Consequuntur sed ipsam et totam...</p>
-            </div>
-
-            <div class="post-item clearfix">
-              <img src="/admin/assets/img/news-4.jpg" alt="">
-              <h4><a href="#">Laborum corporis quo dara net para</a></h4>
-              <p>Qui enim quia optio. Eligendi aut asperiores enim repellendusvel rerum cuder...</p>
-            </div>
-
-            <div class="post-item clearfix">
-              <img src="/admin/assets/img/news-5.jpg" alt="">
-              <h4><a href="#">Et dolores corrupti quae illo quod dolor</a></h4>
-              <p>Odit ut eveniet modi reiciendis. Atque cupiditate libero beatae dignissimos eius...</p>
-            </div>
-
-          </div><!-- End sidebar recent posts-->
-
-        </div>
-      </div><!-- End News & Updates -->
+      <!-- Bordered Table -->
+      <table class="table table-bordered">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Name</th>
+            <th scope="col">Position</th>
+            <th scope="col">Age</th>
+            <th scope="col">Start Date</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Brandon Jacob</td>
+            <td>Designer</td>
+            <td>28</td>
+            <td>2016-05-25</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Bridie Kessler</td>
+            <td>Developer</td>
+            <td>35</td>
+            <td>2014-12-05</td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td>Ashleigh Langosh</td>
+            <td>Finance</td>
+            <td>45</td>
+            <td>2011-08-12</td>
+          </tr>
+          <tr>
+            <th scope="row">4</th>
+            <td>Angus Grady</td>
+            <td>HR</td>
+            <td>34</td>
+            <td>2012-06-11</td>
+          </tr>
+          <tr>
+            <th scope="row">5</th>
+            <td>Raheem Lehner</td>
+            <td>Dynamic Division Officer</td>
+            <td>47</td>
+            <td>2011-04-19</td>
+          </tr>
+        </tbody>
+      </table>
+      <!-- End Bordered Table -->
     </section>
 <script>
   $("#cateup").click(function () {
